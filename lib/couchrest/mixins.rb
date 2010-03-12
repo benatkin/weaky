6682a8f6e515fa8c99afe74b46d82824aa83de9e
ben@benatkin.com <ben@benatkin.com>
2010-03-11 22:04:47 -0700

    removed (incorrectly) vendored couchrest

--- lib/couchrest/mixins.rb
+++
@@ -1,3 +0,0 @@
-mixins_dir = File.join(File.dirname(__FILE__), 'mixins')
-
-require File.join(mixins_dir, 'views')
--- lib/couchrest/mixins/extended_document_mixins.rb
+++
@@ -1,4 +0,0 @@
-require File.join(File.dirname(__FILE__), 'properties')
-require File.join(File.dirname(__FILE__), 'document_queries')
-require File.join(File.dirname(__FILE__), 'extended_views')
-require File.join(File.dirname(__FILE__), 'design_doc')
